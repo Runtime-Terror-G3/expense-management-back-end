fd6c4c76827d2b2f1a6d4b7128b23045a7dc24e4
Leonard Pop <leonard.pop@stud.ubbcluj.ro>
2021-11-05 22:35:29 +0200

    Add finding users by email in repo

--- Repository/src/main/java/repository/IUserRepository.java
+++ Repository/src/main/java/repository/IUserRepository.java
@@ -2,5 +2,17 @@ package repository;
 
 import domain.User;
 
+import java.util.Optional;
+
 public interface IUserRepository extends IRepository<Integer, User> {
+
+    /**
+     * Method for finding a user
+     * @param email: String, email of the desired user
+     * @return an {@code Optional}
+     *          - null, if there is no entity with email equal to {@param email}
+     *          - the entity, otherwise
+     * @throws IllegalArgumentException if email is null
+     */
+    public Optional<User> findByEmail(String email);
 }
--- Repository/src/main/java/repository/hibernate/UserHbRepository.java
+++ Repository/src/main/java/repository/hibernate/UserHbRepository.java
@@ -2,8 +2,11 @@ package repository.hibernate;
 
 import domain.User;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import repository.IUserRepository;
+
+import java.util.Optional;
 
 public class UserHbRepository extends AbstractHbRepository<Integer, User> implements IUserRepository {
     @Override
@@ -15,5 +18,30 @@ public class UserHbRepository extends AbstractHbRepository<Integer, User> implem
     @Override
     protected Query<User> getFindAllQuery(Session session) {
         return session.createQuery("from User", User.class);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        if (email == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Optional<User> result = Optional.empty();
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User entity = session.createQuery("from User where email=:email", User.class)
+                    .setParameter("email", email)
+                    .setMaxResults(1)
+                    .uniqueResult();
+            transaction.commit();
+            result = entity != null ? Optional.of(entity) : result;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            if (transaction != null)
+                transaction.rollback();
+        }
+
+        return result;
     }
 }
