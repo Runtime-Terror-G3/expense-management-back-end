a0ab2e671ae4651035d25f669ae3cd187fd99cc5
Oskar Picus <oskarpicus@yahoo.com>
2021-11-18 17:29:52 +0200

    Implemented DB query for retrieving statistics

--- Repository/src/main/java/repository/IExpenseRepository.java
+++ Repository/src/main/java/repository/IExpenseRepository.java
@@ -1,6 +1,11 @@
 package repository;
 
 import domain.Expense;
+import domain.ExpenseCategory;
+import domain.User;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 public interface IExpenseRepository extends IRepository<Integer, Expense> {
     /**
@@ -12,4 +17,13 @@ public interface IExpenseRepository extends IRepository<Integer, Expense> {
      * @return a collection containing the requested expenses
      */
     Iterable<Expense> findByFilter(int userId, String category, long startDate, long endDate);
+
+    /**
+     * Method for retrieving statistics regarding the total amount of expenses of a user by category within a time period
+     * @param user, the user the statistics are addressed to
+     * @param start, the start of the considered time period
+     * @param end, the end of the considered time period
+     * @return a {@code Map} containing, for each ExpenseCategory present, the total amount
+     */
+    Map<ExpenseCategory, Double> getTotalAmountByCategory(User user, LocalDateTime start, LocalDateTime end);
 }
--- Repository/src/main/java/repository/hibernate/ExpenseHbRepository.java
+++ Repository/src/main/java/repository/hibernate/ExpenseHbRepository.java
@@ -2,18 +2,21 @@ package repository.hibernate;
 
 import domain.Expense;
 import domain.ExpenseCategory;
+import domain.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 import repository.IExpenseRepository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.TimeZone;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class ExpenseHbRepository extends AbstractHbRepository<Integer, Expense> implements IExpenseRepository {
@@ -64,5 +67,57 @@ public class ExpenseHbRepository extends AbstractHbRepository<Integer, Expense> 
                 transaction.rollback();
         }
         return new ArrayList<>();
+    }
+
+    @Override
+    public Map<ExpenseCategory, Double> getTotalAmountByCategory(User user, LocalDateTime start, LocalDateTime end) {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+
+            CriteriaQuery<ExpenseCategoryAmountTuple> criteriaQuery = builder.createQuery(ExpenseCategoryAmountTuple.class);
+            Root<Expense> root = criteriaQuery.from(Expense.class);
+
+            Predicate predicateDate = builder.between(root.get("date"), start, end);
+            Predicate predicateUser = builder.equal(root.<User>get("user"), user);
+
+            criteriaQuery.where(builder.and(predicateUser, predicateDate));
+            criteriaQuery.groupBy(root.get("category"));
+            criteriaQuery.multiselect(root.get("category"), builder.sum(root.get("amount")));
+
+            Query<ExpenseCategoryAmountTuple> query = session.createQuery(criteriaQuery);
+            List<ExpenseCategoryAmountTuple> resultList = query.getResultList();
+
+            return resultList.stream()
+                    .collect(Collectors.toMap(ExpenseCategoryAmountTuple::getCategory, ExpenseCategoryAmountTuple::getAmount));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyMap();
+        }
+    }
+}
+
+class ExpenseCategoryAmountTuple {
+    private final ExpenseCategory category;
+    private final double amount;
+
+    public ExpenseCategoryAmountTuple(ExpenseCategory category, double amount) {
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public ExpenseCategory getCategory() {
+        return category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "ExpenseCategoryAmountTuple{" +
+                "category=" + category +
+                ", amount=" + amount +
+                '}';
     }
 }
