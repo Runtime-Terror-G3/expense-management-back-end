7839872e6c729bc6be49187792bb479559498572
oskarpicus <57806887+oskarpicus@users.noreply.github.com>
2021-12-01 13:11:13 +0200

    Feature/exp category stats (#16)
    * Implemented DB query for retrieving statistics
    
    * Implemented service layer of the functionality
    
    * Implemented controller for feature
    
    * Removed call for finding user when retrieving statistics
    
    * Removed redundant checked exception

--- Service/src/main/java/service/IService.java
+++ Service/src/main/java/service/IService.java
@@ -1,6 +1,7 @@
 package service;
 
 import domain.Expense;
+import domain.ExpenseCategory;
 import domain.TotalExpensesDto;
 import domain.User;
 import dto.ExpenseDto;
@@ -10,17 +11,20 @@ import viewmodel.ExpenseViewModel;
 import viewmodel.MonthlyBudgetViewModel;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Map;
 import java.util.Optional;
 
 public interface IService {
     /**
      * Creates an account
-     * @param email the email to be associated with the account
-     * @param firstName user's first name
-     * @param lastName user's last name
+     *
+     * @param email       the email to be associated with the account
+     * @param firstName   user's first name
+     * @param lastName    user's last name
      * @param dateOfBirth unix time
-     * @param password hex string containing the password
+     * @param password    hex string containing the password
      * @return an Optional
      * - empty if successful
      * - containing the user created by the given parameters, otherwise
@@ -34,9 +38,10 @@ public interface IService {
     Optional<User> getTokenUser(String token);
 
     /**
-        Deletes a monthly budget of a given user
+     * Deletes a monthly budget of a given user
+     *
      * @param budgetId id of the budget to be deleted
-     * @param userId id of the user who requested the deletion
+     * @param userId   id of the user who requested the deletion
      * @return a ServiceEmptyResponse with status=200 in case of success
      * a ServiceEmptyResponse with status=403 in case the budget with
      * the given id is not owned by the user with the given userId
@@ -50,8 +55,9 @@ public interface IService {
 
     /**
      * delete an expense
+     *
      * @param expenseId-id of the expense
-     * @param userId-id of the user who make the request
+     * @param userId-id    of the user who make the request
      * @return-a ServiceEmptyResponse with status:
      * 200-succes
      * 403-the user who make the request isn't the user with this expense
@@ -59,7 +65,8 @@ public interface IService {
      * 404-expense not found
      */
     ExpenseViewModel deleteExpense(int expenseId, int userId) throws ServiceException;
-/*
+
+    /*
      * Get expenses filtered by a date interval
      * @param userId id of the user the expenses belong to
      * @param category category of the expenses
@@ -71,6 +78,16 @@ public interface IService {
     Iterable<Expense> getExpenses(int userId, String category, long startDate, long endDate) throws ServiceException;
 
     /**
+     * Method for retrieving the total amount of a user's expenses within a time period, grouped by expense category
+     *
+     * @param userId, identifier of the user whose expenses are taken into account
+     * @param start,  the start of the considered time period
+     * @param end,    the end of the considered time period
+     * @return a {@code Map} where each key represents an {@code ExpenseCategory} and the value, the total amount of expenses of that category
+     */
+    Map<ExpenseCategory, Double> getExpenseTotalByCategory(int userId, LocalDateTime start, LocalDateTime end);
+
+    /**
      * Updates a monthly budget
      * @param budgetId id of the budget to be updated
      * @param monthlyBudgetDto a dto object of the monthly budget to be updated
@@ -78,7 +95,6 @@ public interface IService {
      * @throws ServiceException if the budget can't be updated
      */
     MonthlyBudgetViewModel updateMonthlyBudget(int budgetId, MonthlyBudgetDto monthlyBudgetDto) throws ServiceException;
-
 
     ExpenseViewModel updateExpense(ExpenseDto updateExpenseDto, int expenseId) throws ServiceException;
 
