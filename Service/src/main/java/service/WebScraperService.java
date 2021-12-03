030f7dee169cf7a7a72cd53523e23b5733befad1
MohiRZV <rsalsigan@gmail.com>
2021-12-03 20:46:45 +0200

    refactor webscraper method to return a WishlistItem with the link, img url, title, price and vendor set

--- Service/src/main/java/service/WebScraperService.java
+++ Service/src/main/java/service/WebScraperService.java
@@ -1,37 +1,56 @@
 package service;
 
+import domain.WishlistItem;
+import domain.WishlistItemVendor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
+import service.exception.ServiceException;
 
 public class WebScraperService {
 
-    public static final String url = "https://altex.ro/laptop-gaming-asus-rog-strix-scar-15-g533qs-hq122-amd-ryzen-9-5900hx-pana-la-4-6ghz-15-6-qhd-32gb-ssd-2tb-nvidia-geforce-rtx-3080-16gb-free-dos-negru/cpd/LAPG533QSHQ122/";
-    //public static final String url ="https://altex.ro/microsistem-audio-panasonic-sc-pm250ecs-20w-bluetooth-usb-cd-radio-fm-argintiu/cpd/MICSCPM250ECS/";
-    public static String getProductsFromAltex(String url) throws IOException {
-        String result = "";
+    /**
+     * Get the details of the product by a given link
+     * @param url the link to the product's page
+     * @return a WishlistItem containing the needed details about the product
+     * @throws ServiceException if something goes wrong while parsing the data (e.g. missing selector, unexpected data format)
+     */
+    public WishlistItem getWishlistItemAltex(String url) throws ServiceException {
+        WishlistItem wishlistItem = new WishlistItem();
+        wishlistItem.setLink(url);
+        wishlistItem.setVendor(WishlistItemVendor.Altex);
+        wishlistItem.setTitle("");
+        try {
+            Document doc = Jsoup.connect(url).get();
+            String title = doc.select("div.mb-5 > div.mb-1 > h1 > div").get(0).text();
 
-        Document doc = Jsoup.connect(url).get();
-        String imageUrl = doc.select("div.slick-slide.slick-active.slick-current").get(0).select("img").attr("src");
-        System.out.println("Image url:");
-        System.out.println(imageUrl);
+            String imageUrl = doc.select("div.slick-slide.slick-active.slick-current").get(0).select("img").attr("src");
 
-        String priceInt = doc.select(".Price-int.leading-none").get(0).text().replace(".","");
-        String priceDec = doc.select(".Price-int.leading-none").next().get(0).text().replace(',','.');
-        String price = priceInt+priceDec;
-        System.out.println("\nPrice:");
-        System.out.println(price);
+            String priceInt = doc.select(".Price-int.leading-none").get(0).text().replace(".", "");
+            String priceDec = doc.select(".Price-int.leading-none").next().get(0).text().replace(',', '.');
+            double price = Double.parseDouble(priceInt + priceDec);
 
-        return result;
+            wishlistItem.setTitle(title);
+            wishlistItem.setPrice(price);
+            wishlistItem.setImage(imageUrl);
+        }catch (Exception exception){
+            throw new ServiceException("Something went wrong while trying to get the product details from Altex!");
+        }
+
+        return wishlistItem;
     }
 
     public static void main(String[] args) {
         try {
-            System.out.println(getProductsFromAltex(url));
-        } catch (IOException e) {
+
+            String[] urls = {
+                    "https://altex.ro/laptop-gaming-asus-rog-strix-scar-15-g533qs-hq122-amd-ryzen-9-5900hx-pana-la-4-6ghz-15-6-qhd-32gb-ssd-2tb-nvidia-geforce-rtx-3080-16gb-free-dos-negru/cpd/LAPG533QSHQ122/",
+                    "https://altex.ro/microsistem-audio-panasonic-sc-pm250ecs-20w-bluetooth-usb-cd-radio-fm-argintiu/cpd/MICSCPM250ECS/",
+                    "https://altex.ro/vin-rosu-sec-domini-veneti-amarone-5l-cutie/cpd/VIN29373/"
+            };
+            for (String url : urls) {
+                System.out.println(new WebScraperService().getWishlistItemAltex(url));
+            }
+        } catch (ServiceException e) {
             e.printStackTrace();
         }
     }
