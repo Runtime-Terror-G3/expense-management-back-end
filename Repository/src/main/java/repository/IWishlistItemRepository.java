4e111ba706cdb4202754daf9af20a20b6f9975f1
Alexandra <alexandra.birle@yahoo.com>
2021-12-08 18:19:36 +0200

    implemented get affordable items from wishlist

--- Repository/src/main/java/repository/IWishlistItemRepository.java
+++ Repository/src/main/java/repository/IWishlistItemRepository.java
@@ -5,4 +5,5 @@ import domain.WishlistItem;
 public interface IWishlistItemRepository extends IRepository<Integer, WishlistItem>{
 
     Iterable<WishlistItem> findByUser(int userId);
+    Iterable<WishlistItem> getAffordableWishlistItems(int userId);
 }
