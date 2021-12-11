48d42e5fbdd55c6e10bbaf8c5a65d4be6d2c5d0d
Leonard Pop <leonard.pop@stud.ubbcluj.ro>
2021-12-11 22:31:12 +0200

    Fix authorization exception handling

--- Rest/src/main/java/controller/GlobalExceptionHandler.java
+++ Rest/src/main/java/controller/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import utils.AuthorizationException;
+import service.exception.AuthorizationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -19,19 +19,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     ){
         System.out.println("An exception occurred and it was handled by the global exception handler");
         ex.printStackTrace();
+        HttpStatus statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        String message = "Internal server error";
 
         if(ex instanceof AuthorizationException) {
             AuthorizationException authorizationException = (AuthorizationException) ex;
-            HttpStatus statusCode;
-            String message;
 
             switch(authorizationException.code) {
                 case FORBIDDEN:
                     message = "Access denied";
                     statusCode = HttpStatus.FORBIDDEN;
+                    break;
                 case UNAUTHORIZED:
                     message = "Not logged in";
                     statusCode = HttpStatus.UNAUTHORIZED;
+                    break;
                 default:
                     message = "Authorization error";
                     statusCode = HttpStatus.UNAUTHORIZED;
@@ -40,19 +42,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             if(authorizationException.getMessage() != null) {
                 message = authorizationException.getMessage();
             }
-
-            handleExceptionInternal(
-                    ex,
-                    message,
-                    new HttpHeaders(),
-                    statusCode,
-                    request);
         }
 
         return handleExceptionInternal(
                 ex,
-                "Internal Server Error",
+                message,
                 new HttpHeaders(),
-                HttpStatus.INTERNAL_SERVER_ERROR, request);
+                statusCode,
+                request);
     }
 }
